25cfb41c03f3b35f54be08ad653c27189f358b57
PaulaRRP <paularincorp@gmail.com>
2024-04-24 09:46:03 -0300

    adding out_MA parameters and chart

--- src/main_sa.py
+++ src/main_sa.py
@@ -19,17 +19,21 @@ parametersDictionary = {
   'n_610': 34.8,
   'n_66': 560,
   'n_6TNF': 185,
-  'k_6': 4.64,
-  'k_6m': 0.01,
-  'k_6TNF': 0.81,
+  'n_MTNF': 0.1,
   'h_610': 4,
   'h_66': 1,
   'h_6TNF': 2,
+  'h_MTNF': 3.16,
+  'k_6': 4.64,
+  'k_6m': 0.01,
+  'k_6TNF': 0.81,
+  'k_MTNF': 8.65, #da erro pra rodar a SA
   'q_IL6': 0.6, #da erro pra rodar a SA
 
   'beta_A': 0.02, #da erro pra rodar a SA
   'k_A':50, #da erro pra rodar a SA
   'm_A': 0.9, #da erro pra rodar a SA
+  'k_m': 1.414, #da erro pra rodar a SA
 }
 
 def parametersInterval(parameters):
@@ -44,20 +48,22 @@ def parametersInterval(parameters):
 
 def citokynes(cortisol_parameters, brady_parameters, quintela_parameters):
   simulation = 'F'
+
   output = cdd.cortisolDecadesOneDay(simulation, cortisol_parameters, brady_parameters, quintela_parameters, cortisol_exp=2.32)
+
   [t_wcsa, outputs_wcsa] = output
   [out_A, out_MA, out_MR, out_IL10, out_IL6, out_IL8, out_TNF, out_COR] = outputs_wcsa
 
   #nenhum parametro do out_A funcionou na SA
+
   size = np.size(out_MA)
   return out_MA[math.ceil(size/2)]
-  #return t_wcsa, out_IL6
 
 
 if __name__ == "__main__":
     start = time.time()
 
-    names = ['n_610', 'n_66', 'n_6TNF', 'h_610', 'h_66', 'h_6TNF', 'k_6', 'k_6m', 'k_6TNF', 'ktc', 'kmtc', 'kmct', 'm_A']
+    names = ['n_610', 'n_66', 'n_6TNF', 'n_MTNF', 'h_610', 'h_66', 'h_6TNF', 'h_MTNF', 'k_6', 'k_6m', 'k_6TNF', 'ktc', 'kmtc', 'kmct']
     problem = {
         'num_vars': np.size(names),
         'names': names,
@@ -68,10 +74,11 @@ if __name__ == "__main__":
     model_values = np.zeros(param_values.shape[0])
 
     for i, X in enumerate(param_values):
-        [n_610, n_66, n_6TNF, h_610, h_66, h_6TNF, k_6, k_6m, k_6TNF, ktc, kmtc, kmct, m_A] = X
+        [n_610, n_66, n_6TNF, n_MTNF, h_610, h_66, h_6TNF, h_MTNF, k_6, k_6m, k_6TNF, ktc, kmtc, kmct] = X
+
         cortisol_parameters = [ktc, kmtc, kmct]
-        brady_parameters = [n_610, n_66, n_6TNF, h_610, h_66, h_6TNF, k_6, k_6m, k_6TNF]
-        quintela_parameters = [m_A]
+        brady_parameters = [n_610, n_66, n_6TNF, n_MTNF, h_610, h_66, h_6TNF, h_MTNF, k_6, k_6m, k_6TNF]
+        quintela_parameters = []
 
         model_values[i] = citokynes(cortisol_parameters, brady_parameters, quintela_parameters)
 
@@ -88,7 +95,7 @@ if __name__ == "__main__":
     # total_Si, first_Si = Si.to_df()
   
     Si.plot()
-    filename = 'sobol_analysis_A.png'
+    filename = 'sobol_analysis_MA.png'
     plt.savefig(filename)
 
     end = time.time()
